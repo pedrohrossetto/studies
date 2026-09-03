0eceea53b462ef7bf540112126cb26289a5bd5ff
Pedro Henrique <pedrohcrossetto@gmail.com>
2026-09-03 16:35:09 -0300

    refact(ed2): refazer menu dinamico com lista encadeada e eliminar bibliotecas avanacadas

--- college/ifsul/bcc/semestre4/estrutura-dados/exec_menu/bst.cpp
+++ college/ifsul/bcc/semestre4/estrutura-dados/exec_menu/bst.cpp
@@ -3,7 +3,6 @@
 #include "../modules/menu_funcoes.hpp"
 #include "../modules/utils_plus.hpp"
 #include <iostream>
-#include <vector>
 
 
 
@@ -12,34 +11,34 @@ int main() {
     TreeNode* root = nullptr;
     bool rodando = true;
 
-    std::vector<MenuAcao> acoes;
+    ListaMenuAcao acoes = lista_criar();
 
-    acoes.push_back({"Sair", [&]() {
+    lista_adicionar(acoes, {"Sair", [&]() {
         rodando = false;
     }});
 
-    acoes.push_back({"Inserir", [&]() {
+    lista_adicionar(acoes, {"Inserir", [&]() {
         int val = lerInteiroValido("Valor: ");
         tree_insert(root, val);
         std::cout << "Inserido.\n";
     }});
 
-    acoes.push_back({"Inserir randomizada", [&]() {
+    lista_adicionar(acoes, {"Inserir randomizada", [&]() {
         int lim_min = lerInteiroValido("Limite Menor: ");
-        int lim_max = lerInteiroValido(("Limite Maior: "));
-        tree_insert_random(root, lim_min,lim_max);
+        int lim_max = lerInteiroValido("Limite Maior: ");
+        tree_insert_random(root, lim_min, lim_max);
         std::cout << "Inserido.\n";
     }});
 
-    acoes.push_back({"Inserir lista randomizada", [&]() {
+    lista_adicionar(acoes, {"Inserir lista randomizada", [&]() {
         int qtd_val = lerInteiroValido("Quantidade de inserções: ");
         int lim_min = lerInteiroValido("Limite Menor: ");
-        int lim_max = lerInteiroValido(("Limite Maior: "));
-        tree_fill_random(root,qtd_val, lim_min,lim_max);
+        int lim_max = lerInteiroValido("Limite Maior: ");
+        tree_fill_random(root, qtd_val, lim_min, lim_max);
         std::cout << "Inserido.\n";
     }});
 
-    acoes.push_back({"Buscar nó", [&]() {
+    lista_adicionar(acoes, {"Buscar nó", [&]() {
         int val = lerInteiroValido("Valor: ");
         if (node_search(root, val)) {
             std::cout << "Encontrado.\n";
@@ -48,7 +47,7 @@ int main() {
         }
     }});
 
-    acoes.push_back({"Buscar nó e sua altura", [&]() {
+    lista_adicionar(acoes, {"Buscar nó e sua altura", [&]() {
         int val = lerInteiroValido("Valor: ");
         int height = node_height_search(root, val);
         if (height != -1) {
@@ -58,73 +57,71 @@ int main() {
         }
     }});
 
-    acoes.push_back({"Remover Node", [&]() {
+    lista_adicionar(acoes, {"Remover Node", [&]() {
         int val = lerInteiroValido("Valor: ");
         node_delete(root, val);
         std::cout << "Remoção tentada.\n";
     }});
 
-    acoes.push_back({"Limpar Árvore", [&](){
+    lista_adicionar(acoes, {"Limpar Árvore", [&]() {
         tree_clear(root);
         std::cout << "Árvore Excluída.\n";
-        }});
+    }});
 
-
-    acoes.push_back({"Exibir in-ordem", [&]() {
+    lista_adicionar(acoes, {"Exibir in-ordem", [&]() {
         tree_walk_inorder(root);
         std::cout << "\n";
     }});
 
-    acoes.push_back({"Exibir pré-ordem", [&]() {
+    lista_adicionar(acoes, {"Exibir pré-ordem", [&]() {
         tree_walk_preorder(root);
         std::cout << "\n";
     }});
 
-    acoes.push_back({"Exibir pós-ordem", [&]() {
+    lista_adicionar(acoes, {"Exibir pós-ordem", [&]() {
         tree_walk_postorder(root);
         std::cout << "\n";
     }});
 
-    acoes.push_back({"Predecessor do nó",[&](){
+    lista_adicionar(acoes, {"Predecessor do nó", [&]() {
         std::cout << node_predecessor(root)->val;
     }});
 
-    acoes.push_back({"Sucessor do nó",[&](){
+    lista_adicionar(acoes, {"Sucessor do nó", [&]() {
         std::cout << node_sucessor(root)->val;
     }});
 
-    acoes.push_back({"Maior Valor da Árvore",[&](){
+    lista_adicionar(acoes, {"Maior Valor da Árvore", [&]() {
         std::cout << tree_max(root)->val;
     }});
 
-    acoes.push_back({"Menor Valor da Árvore",[&](){
+    lista_adicionar(acoes, {"Menor Valor da Árvore", [&]() {
         std::cout << tree_min(root)->val;
     }});
 
-    acoes.push_back({"Soma dos elementos da Árvore",[&](){
+    lista_adicionar(acoes, {"Soma dos elementos da Árvore", [&]() {
         std::cout << tree_sum(root);
     }});
 
-    acoes.push_back({"Soma dos elementos da Árvore (condicional)",[&](){
+    lista_adicionar(acoes, {"Soma dos elementos da Árvore (condicional)", [&]() {
         int min = lerInteiroValido("Informe o limite minimo do intervalo de soma: ");
         int max = lerInteiroValido("Informe o limite máximo do intervalo de soma: ");
-        std::cout << tree_sum_conditional(root,min,max);
+        std::cout << tree_sum_conditional(root, min, max);
     }});
 
-    acoes.push_back({"Altura da Árvore",[&](){
+    lista_adicionar(acoes, {"Altura da Árvore", [&]() {
         std::cout << tree_height(root);
     }});
 
-    acoes.push_back({"Tamanho da Árvore",[&](){
+    lista_adicionar(acoes, {"Tamanho da Árvore", [&]() {
         std::cout << tree_size(root);
     }});
 
-    acoes.push_back({"Estatísticas Compiladas", [&]() {
+    lista_adicionar(acoes, {"Estatísticas Compiladas", [&]() {
         tree_stats(root);
     }});
 
-
-    acoes.push_back({"Exportar DOT/PNG", [&]() {
+    lista_adicionar(acoes, {"Exportar DOT/PNG", [&]() {
         export_to_dot(root, "arvore.dot");
     }});
 
@@ -143,5 +140,6 @@ int main() {
     }
 
     tree_clear(root);
+    lista_liberar(acoes);
     return 0;
 }
--- college/ifsul/bcc/semestre4/estrutura-dados/modules/menu_funcoes.hpp
+++ college/ifsul/bcc/semestre4/estrutura-dados/modules/menu_funcoes.hpp
@@ -3,26 +3,75 @@
 
 #include <functional>
 #include <iostream>
-#include <string>
-#include <vector>
 
 struct MenuAcao {
-    std::string nome;
+    const char* nome;
     std::function<void()> executar;
 };
 
-static void menu_listar(const std::vector<MenuAcao>& acoes) {
-    for (size_t i = 0; i < acoes.size(); ++i) {
-        std::cout << i << " - " << acoes[i].nome << "\n";
+struct NodoMenu {
+    MenuAcao acao;
+    NodoMenu* proximo;
+};
+
+struct ListaMenuAcao {
+    NodoMenu* cabeca;
+    int tamanho;
+};
+
+static ListaMenuAcao lista_criar() {
+    ListaMenuAcao lista;
+    lista.cabeca  = nullptr;
+    lista.tamanho = 0;
+    return lista;
+}
+
+static void lista_adicionar(ListaMenuAcao& lista, MenuAcao acao) {
+    NodoMenu* novo = new NodoMenu;
+    novo->acao     = acao;
+    novo->proximo  = nullptr;
+
+    if (lista.cabeca == nullptr) {
+        lista.cabeca = novo;
+    } else {
+        NodoMenu* atual = lista.cabeca;
+        while (atual->proximo != nullptr)
+            atual = atual->proximo;
+        atual->proximo = novo;
+    }
+    lista.tamanho++;
+}
+
+static void menu_listar(const ListaMenuAcao& lista) {
+    NodoMenu* atual = lista.cabeca;
+    int i = 0;
+    while (atual != nullptr) {
+        std::cout << i << " - " << atual->acao.nome << "\n";
+        atual = atual->proximo;
+        i++;
     }
 }
 
-static void menu_executar(const std::vector<MenuAcao>& acoes, int indice) {
-    if (indice < 0 || indice >= static_cast<int>(acoes.size())) {
+static void menu_executar(const ListaMenuAcao& lista, int indice) {
+    if (indice < 0 || indice >= lista.tamanho) {
         std::cout << "Índice fora da lista.\n";
         return;
     }
-    acoes[indice].executar();
+    NodoMenu* atual = lista.cabeca;
+    for (int i = 0; i < indice; i++)
+        atual = atual->proximo;
+    atual->acao.executar();
+}
+
+static void lista_liberar(ListaMenuAcao& lista) {
+    NodoMenu* atual = lista.cabeca;
+    while (atual != nullptr) {
+        NodoMenu* proximo = atual->proximo;
+        delete atual;
+        atual = proximo;
+    }
+    lista.cabeca  = nullptr;
+    lista.tamanho = 0;
 }
 
 #endif
